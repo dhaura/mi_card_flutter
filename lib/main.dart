d74d1fd31ae8e5fcda7df36db904ad3fe28c1c07
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-06-08 23:45:22 +0530

    Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(20.0, 50.0, 30.0, 10.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
